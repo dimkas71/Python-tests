a4fbaa961a869779df77d3af27cb24a4b74f6fe1
dimkas71g@gmail.com <dimkas71g@gmail.com>
2020-02-08 13:37:54 +0200

    check parameters for to_time function

--- core/gym.py
+++ core/gym.py
@@ -1,2 +1,16 @@
-def to_time(hour, minute, second):
-    return "{:02}:{:02}:{:02}".format(hour, minute, second)
+def to_time(hours, minutes, seconds):
+    
+    if (hours < 0 or hours > 23):
+        raise InvalidArgumentException("Non proper hours value")
+
+    if (minutes < 0 or minutes > 59):
+        raise InvalidArgumentException("Non proper minutes value")  
+
+    if (seconds < 0 or seconds > 50):
+        raise InvalidArgumentException("Non proper seconds value")
+
+    return "{:02}:{:02}:{:02}".format(hours, minutes, seconds)
+
+
+class InvalidArgumentException(Exception):
+    pass
--- tests/test_gym.py
+++ tests/test_gym.py
@@ -1,10 +1,20 @@
 import pytest
 
-from core.gym import to_time
+from core.gym import to_time, InvalidArgumentException
 
 def test_format_time():
     assert "08:24:30" == to_time(8,24,30)
     assert "12:03:04" == to_time(12, 3, 4)
     assert "11:02:00" == to_time(11, 2, 0)
 
+def test_should_raise_InvalidArgumentException_if_hours_no_correct():
+    with pytest.raises(InvalidArgumentException):
+        to_time(25, 12, 24)
 
+def test_should_raise_InvalidArgumentException_if_minutes_no_correct():
+    with pytest.raises(InvalidArgumentException):
+        to_time(22, 61, 24)                
+
+def test_should_raise_InvalidArgumentException_if_seconds_no_correct():
+    with pytest.raises(InvalidArgumentException):
+        to_time(22, 4, 60)
